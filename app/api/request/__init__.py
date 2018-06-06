86601e4034e7abeddf64833e9e22d1fae9f8c92e
Kyampeire Hadijah <Hadijahk@Erics-MacBook-Pro.local>
2018-06-06 16:56:26 +0300

    modify endpoints

--- app/api/request/__init__.py
+++ app/api/request/__init__.py
@@ -4,7 +4,7 @@ from .views import AddRequest, EditRequest, GetRequests, GetOneRequest
 
 request = Blueprint('requests', __name__)
 maintainance_api = Api(request)
-maintainance_api.add_resource(AddRequest, '/api/v1/requests')
-maintainance_api.add_resource(GetRequests, '/api/v1/requests')
-maintainance_api.add_resource(GetOneRequest, '/api/v1/requests/<request_id>')
-maintainance_api.add_resource(EditRequest, '/api/v1/requests/<request_id>')
+maintainance_api.add_resource(AddRequest, '/api/v1/users/requests')
+maintainance_api.add_resource(GetRequests, '/api/v1/users/requests')
+maintainance_api.add_resource(GetOneRequest, '/api/v1/users/requests/<request_id>')
+maintainance_api.add_resource(EditRequest, '/api/v1/users/requests/<request_id>')
