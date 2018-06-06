ea5184cfb15be303b29dd5cce9b37e0feed2d39b
Kyampeire Hadijah <Hadijahk@Erics-MacBook-Pro.local>
2018-06-06 17:08:19 +0300

    Add procfile

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from app.api.user import views
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Welcome"
+    return "Welcome try the other endpoints in postman, find the repo here https://github.com/hadijahkyampeire/Maintainance-Tracker"
 
 
 if __name__ == '__main__':
